138a3f28efe2cf9aecc673a08113659204610e4e
chpengzh <chpengzh@foxmail.com>
2019-06-06 20:29:59 +0800

    [#66] Fix metrix server and gateway server bootstrap (#68)

--- boot/all-in-one-bootstrap/build.sh
+++ boot/all-in-one-bootstrap/build.sh
@@ -2,4 +2,4 @@
 
 current_dir=$( dirname $0)
 cd $current_dir/../..
-mvn clean install -T 1C -Psofaark -Dmaven.test.skip=true -am -pl boot/all-in-one-bootstrap
+mvn clean install -T 1C -Psofaark -P!single-bootstrap -Dmaven.test.skip=true -am -pl boot/all-in-one-bootstrap
